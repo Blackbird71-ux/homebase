0f3c568b702cb7fbe8fb35ba84bd531f9bf784b2
Blackbird71-ux <mark.a.liddle@gmail.com>
2026-04-19 07:16:22 +1000

    feat: add recipeId, recipeName, categoryOrder fields for shopping list UX

---
+++ prisma/migrations/20260418211613_add_shopping_ux_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "List" ADD COLUMN "categoryOrder" TEXT;
+
+-- AlterTable
+ALTER TABLE "ListItem" ADD COLUMN "recipeId" TEXT;
+ALTER TABLE "ListItem" ADD COLUMN "recipeName" TEXT;
