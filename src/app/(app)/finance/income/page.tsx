bcc3d6700df84f34a1a9b982be1d2d09b950857e
Blackbird71-ux <mark.a.liddle@gmail.com>
2026-05-13 22:36:46 +1000

    fix: add missing DialogContent import in income page

--- src/app/(app)/finance/income/page.tsx
+++ src/app/(app)/finance/income/page.tsx
@@ -14,6 +14,7 @@ import { toMonthlyAmount, formatCurrency } from '@/lib/financeShared'
 import Link from 'next/link'
 import {
   Dialog,
+  DialogContent,
   DialogHeader,
   DialogTitle,
   DialogFooter,
